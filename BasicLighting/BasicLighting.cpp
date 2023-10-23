9ebb01baba6cb83a28a8cc730677f6b9931e0c08
AliMctavish <alifadil4141@gmail.com>
2023-10-23 23:09:36 +0300

    sliders with cube

--- BasicLighting/BasicLighting.cpp
+++ BasicLighting/BasicLighting.cpp
@@ -10,7 +10,7 @@
 #define WINDOW_HEIGHT 800
 #define WINDOW_WIDTH  1200
 #define Model_Default_Position glm::vec3(3,1,5)
-#define Model_To_Ground glm::vec3(1,0,0)
+#define Model_To_Ground glm::vec3(0,1,0)
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
@@ -54,45 +54,76 @@ int main(void)
 
 	glEnable(GL_DEPTH_TEST);
 
-	float textureValue00;
-	float textureValue01;
-	float textureValue23;
-	float textureValue2;
-	float textureValue3;
 
 	//HERE IS THE DRAWING DETAILS
-	float vertices[] =
-	{
    // positions          // colors           // texture coords
-	 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-	 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-	-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-	-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+	 float vertices[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
-	unsigned int indecies[] =
-	{
-		0,1,2,
-		2,0,3,
-	};
+	//unsigned int indecies[] =
+	//{
+	//	0,1,2,
+	//	2,0,3,
+	//};
 
 	VertexArray vertexArray;
 
 
-	unsigned int EAO;
-	glGenBuffers(1, &EAO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EAO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indecies), &indecies, GL_STATIC_DRAW);
+	//unsigned int EAO;
+	//glGenBuffers(1, &EAO);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EAO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indecies), &indecies, GL_STATIC_DRAW);
 
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)0);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(3 * sizeof(float)));
+	// texture coord attribute
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
 
 
 	Shader shader("VertexShader.shader", "FragmentShader.shader");
@@ -116,6 +147,7 @@ int main(void)
 
 	int num = 1;
 	float resize = 1;
+	float rotate = 1;
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0, 0, 0, 5);
@@ -128,11 +160,11 @@ int main(void)
 		//CREATING A DRAWALBE OBJECT	
 		glm::mat4 model = glm::mat4(1.0f);
 		model = glm::translate(model, glm::vec3(num,num,num));
-		model = glm::rotate(model,glm::radians(90.0f), Model_To_Ground);
+		model = glm::rotate(model,glm::radians(rotate), Model_To_Ground);
 		model = glm::scale(model, glm::vec3(resize));
 		int modelLoc = glGetUniformLocation(shader.shader_program, "model");
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES,0,36);
 		//CREATING A DRAWALBE OBJECT	
 
 		ImGui_ImplOpenGL3_NewFrame();
@@ -150,7 +182,8 @@ int main(void)
 		if (ImGui::Button("Resize -", ImVec2(150, 50)))
 			resize--;
 
-
+			ImGui::SliderFloat("Rotate", &rotate, 0.f, 360.f, "%.3f", 0);
+			ImGui::SliderFloat("MySlider", &resize, 0.f, 2.f, "%.1f", 0);
 		ImGui::End();
 
 		ImGui::Render();

295b011a2d53b18475496b0ca32f85706f076793
ali fadil <alifadil4141@gmail.com>
2023-11-25 08:30:52 +0300

    everything is awesome !

--- BasicLighting/Renderer.cpp
+++ BasicLighting/Renderer.cpp
@@ -108,8 +108,6 @@ void Renderer::Update()
 
 		if (gameStarted == true)
 			_physics.CheckCollision(cube, cameraPos, deltaTime);
-
-		
 		
 		cube.Draw();
 	}
@@ -123,7 +121,7 @@ void Renderer::Update()
 	for (int i = 0; i < lights.size(); i++)
 	{
 		lights[i].Update();
-		//lights[i].SinMove();
+		lights[i].SinMove();
 		lightShader.SetMat4("model", lights[i].GetModel());
 		lights[i].UseColor("objectColor");
 		lights[i].Draw();
